e5f22cc6e61226744deff3e6c44ce38543d210f9
Siddharth <siddharatha.n@gmail.com>
2014-09-21 07:41:42 +0200

    recent code changes

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 salesforcesearchbox
 ===================
 
-source code for salesforce search box plugin 
-This plugin is developed using crossrider.com platform, it injects jQuery to the browser and lets us manipulate the content of the page.
 
-link to plugin : https://chrome.google.com/webstore/detail/salesforce-search-box/moaokcjghnjifeeahifofckbmhofjdbi?hl=en
+A simple search box on top of all configuration items of salesforce setup items.
+Enough time is wasted on finding the class and label by creating views or searching for the same type of fields within an object .
 
+Tired of pressing more link every time ?
+
+I have a solution , try this google chrome [extension](https://chrome.google.com/webstore/detail/salesforce-search-box/moaokcjghnjifeeahifofckbmhofjdbi?hl=en) , it brings in a simple search box, which will search in all configuration items on the page.
+takes care of pressing the more link.
+
+The plugin supports some basic regex searches 
+ex: OR search - formula|lookup
+
+```
+AND search - 
+^(?=.*\text1\b)(?=.*\text2\b)
+```
+
+Don't forget to press "Enter" to search and "Esc" to cancel.
+
+
+link to plugin 
+---------------
+https://chrome.google.com/webstore/detail/salesforce-search-box/moaokcjghnjifeeahifofckbmhofjdbi?hl=en
+
+developer guide
+---------------
+https://developer.chrome.com/extensions/devguide
--- background.js
+++ background.js
@@ -1,8 +1,148 @@
-/************************************************************************************
-  This is your background code.
-  For more information please visit our wiki site:
-  http://docs.crossrider.com/#!/guide/background_scope
-*************************************************************************************/
+  //standard functions
+
+  $.expr[":"].containsNoCase = function(el, i, m) {
+      var search = m[3];
+      if (!search) return false;
+      return eval("/" + search + "/i").test($(el).text());
+  }
 
 
-// Place your code here (ideal for handling browser button, global timers, etc.)
+  // get parameter by name
+  function getParameterByName(name,mylocation)
+  {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(mylocation);
+    if(results == null)
+      return '';
+    else
+      return decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
+  function SalesforceSearchBox(searchboxid) {
+    this.searchboxid = searchboxid;
+    this.searchelement = $('<p/>', {id: searchboxid});
+  }
+
+
+  function SalesforceURLManager(pages,sfclasses,triggers,sfcomponents,generalpage,generallist,rowcount){
+    this.currenturl=$(location).attr('href');
+    this.parametersurl=window.location.search;
+
+      this.apexPagesrowVar=pages;
+      this.apexClassesrowVar=sfclasses;
+      this.triggersrowVar=triggers;
+      this.componentsrowVar=sfcomponents;
+
+      this.ingeneralrowVar=generalpage;
+      this.ingenerallistVar=generallist;
+
+      this.ingeneralrowVar = generalpage,
+      this.ingenerallistVar = generallist
+      this.noofrows = rowcount;
+  }
+
+  SalesforceURLManager.prototype.updateLinks=function(){
+    var currentContext=this;
+    if(getParameterByName('setupid',currentContext.parametersurl))
+    {
+      $('a[id*="_font"]').each(function(){
+      var myHref=$(this).attr('href');
+      if($(this).html()=='Apex Classes'){
+        if(getParameterByName(currentContext.apexClassesrowVar,myHref) === '')
+          $(this).attr('href',myHref+'&'+encodeURIComponent(currentContext.apexClassesrowVar)+'='+currentContext.noofrows);
+      }
+      else if($(this).html()=='Pages'){
+        if(getParameterByName(currentContext.apexPagesrowVar,myHref) === '')
+          $(this).attr('href',myHref+'&'+encodeURIComponent(currentContext.apexPagesrowVar)+'='+currentContext.noofrows);
+      }
+      else if($(this).html()=='Apex Triggers'){
+        if(getParameterByName(currentContext.triggersrowVar,myHref) === '')
+          $(this).attr('href',myHref+'&'+encodeURIComponent(currentContext.triggersrowVar)+'='+currentContext.noofrows);
+      }
+      else if($(this).html()=='Components'){
+        if(getParameterByName(currentContext.componentsrowVar,myHref) === '')
+          $(this).attr('href',myHref+'&'+encodeURIComponent(currentContext.componentsrowVar)+'='+currentContext.noofrows);
+      }
+      else{
+        if(getParameterByName(currentContext.ingenerallistVar,myHref) === '')
+          $(this).attr('href',myHref+'&'+encodeURIComponent(currentContext.ingenerallistVar)+'='+currentContext.noofrows);
+
+        if(getParameterByName(currentContext.ingeneralrowVar,myHref) === '')
+          $(this).attr('href',myHref+'&'+encodeURIComponent(currentContext.ingeneralrowVar)+'='+currentContext.noofrows);
+      }
+    });
+  }
+};
+
+SalesforceSearchBox.prototype.resetSearch = function(){
+      // clear the textbox
+      $('#txtSearch').val('');
+      // show all table rows
+      $('table.list tr').show();
+      // remove any no records rows
+      $('.norecords').remove();
+      // remove the cancel search image
+      $('#imgSearch').hide();
+      // make sure we re-focus on the textbox for usability
+      $('#txtSearch').focus();
+};
+
+SalesforceSearchBox.prototype.search = function(){
+  var currentContext=this;
+      if ($('#txtSearch').val().length > 2) {
+        // hide all rows
+        $('table.list tr').hide();
+        // show the header row
+        $('table.list tr:first').show();
+        // show the matching rows (using the containsNoCase from Rick Strahl)
+        $('table.list tr td:containsNoCase(\'' +$('#txtSearch').val()+ '\')').parent().show();
+        $('table.list tr th:containsNoCase(\'' +$('#txtSearch').val()+ '\')').parent().show();
+      }
+      else if ($('#txtSearch').val().length == 0) {
+        // if the user removed all of the text, reset the search
+        currentContext.resetSearch();
+      }
+
+      // if there were no matching rows, tell the user
+      if ($('table.list tr:visible').length == 1) {
+        // remove the norecords row if it already exists
+        $('.norecords').remove();
+        // add the norecords row
+        $('table.list').append('<tr class="norecords"><td colspan="5" class="Normal">No records were found</td></tr>');
+      }
+  };
+
+  SalesforceURLManager.prototype.isValidToShow=function(){
+       if(this.currenturl.indexOf('setupid=')!=-1 && $('.setupBlock').length>0)
+        return true;
+      else
+        return false;
+  }
+
+  SalesforceSearchBox.prototype.addToBody = function () {
+  var currentContext=this;
+      if ($('#' + currentContext.searchboxid).length === 0) {
+        currentContext.searchelement.css({ "width" : "500px"}).html('<input type="text" id="txtSearch" name="txtSearch" maxlength="50" placeholder="Search" />');
+        $('.setupBlock:first').parent().prepend(currentContext.searchelement);
+         $('#txtSearch').focus();
+         $('#txtSearch').keyup(function(event) {
+            if (event.keyCode == 27) {
+              currentContext.resetSearch();
+            }
+            if(event.keyCode==13){
+              currentContext.search();
+            }
+          });
+      }
+  };
+
+
+//events as follows
+sfurlmanager = new SalesforceURLManager('j_id0:theTemplate:j_id8:rowsperpage','all_classes_page:theTemplate:classList:rowsperpage','all_triggers_page:theTemplate:j_id22:rowsperpage','j_id0:theTemplate:j_id8:rowsperpage','rowsperpage','rowsperlist',10000);
+sfsearchbox = new SalesforceSearchBox('searchboxid_sid');
+
+sfurlmanager.updateLinks();
+if(sfurlmanager.isValidToShow())
+  sfsearchbox.addToBody();
